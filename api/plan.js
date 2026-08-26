abe96b1c0ee52349d35e74ad21c640066976ccaf
jiyo2 <sasuomr7@gmail.com>
2026-08-26 13:38:52 +0300

    Update plan.js

--- api/plan.js
+++ api/plan.js
@@ -1,4 +1,6 @@
-export default async function handler(req, res) {
+console.log("PLAN.JS NEW VERSION RUNNING");
+
+export default async function handler(req, res) {export default async function handler(req, res) {
   if (req.method !== "POST") {
     return res.status(405).json({
       error: "Method not allowed"
